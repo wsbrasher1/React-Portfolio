6746f04daf3a5843768a473ff0d9c544641786bd
Bill Brasher <wsbrasher1@msn.com>
2019-08-21 22:07:06 -0500

    Updates

---
+++ precache-manifest.f32a53bbb44ee147f9f20c40fe302315.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f300e74b5b136770b064455d2f78f534",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "ca021598f41b505a5fcd",
+    "url": "/React-Portfolio/static/css/main.37c0d1c6.chunk.css"
+  },
+  {
+    "revision": "ede4107fe378b4633e87",
+    "url": "/React-Portfolio/static/js/2.c1678298.chunk.js"
+  },
+  {
+    "revision": "ca021598f41b505a5fcd",
+    "url": "/React-Portfolio/static/js/main.628bea21.chunk.js"
+  },
+  {
+    "revision": "a5982163e5c3e3252977",
+    "url": "/React-Portfolio/static/js/runtime~main.982e8b01.js"
+  }
+]);
